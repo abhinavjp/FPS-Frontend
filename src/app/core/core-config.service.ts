4402a235339330f139e3a2728ae25dc7bc714231
Abhinav <abhinavpisharody.asia@gmail.com>
2017-02-06 01:13:39 +0530

    Abhinav: Rebooting Project - 1: Cleaning Old

--- src/app/core/core-config.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class CoreConfigService {
-    apiUrl: string;
-    constructor() {
-        this.apiUrl = 'http://localhost:5000/api/';
-    }
-}
